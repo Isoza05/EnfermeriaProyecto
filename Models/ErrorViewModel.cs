588d5a8df4cdca2310b3caae0cc3fba95ec8bc9e
Pc <Pc@DESKTOP-2A98RBU>
2025-06-24 12:08:55 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EnfermeriaProyectoFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
